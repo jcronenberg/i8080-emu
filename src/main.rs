770a20c568fc9c01bfdbb2ddaa6a9dd1317039fa
Jorik Cronenberg <jcronenberg@suse.de>
2020-05-24 20:12:53 +0200

    Basic hexdump

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("invaders/invaders")?;
+
+    let mut buffer = Vec::new();
+
+    f.read_to_end(&mut buffer)?;
+
+    for (i, v) in buffer.iter().enumerate() {
+        if i % 16 == 0 {
+            print!("{:04x} ", i);
+        }
+        print!("{:02x} ", v);
+        if i % 16 == 15 {
+            print!("\n");
+        }
+    }
+
+    Ok(())
 }
